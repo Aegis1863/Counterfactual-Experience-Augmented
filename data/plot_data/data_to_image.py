d8593ad21b6d1f35b1fd59017a477e4188c9ab8f
Aegis1863 <bw1863@outlook.com>
2024-06-14 19:20:11 +0800

    删除检查点

--- data/plot_data/data_to_image.py
+++ data/plot_data/data_to_image.py
@@ -1,17 +1,24 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import numpy as np
 import os
 
 def smooth(data: pd.Series):
-    smooth_data = data.ewm(alpha=0.1).mean()
+    smooth_data = pd.Series(data).ewm(alpha=0.1).mean()
     return smooth_data
+
+def cumulative_mean(data):
+    data = np.array(data)  # 将输入数据转换为numpy数组
+    cumulative_sum = np.cumsum(data)  # 计算累积和
+    cumulative_mean = cumulative_sum / np.arange(1, len(data) + 1)  # 计算累积均值
+    return cumulative_mean
 
 colors = ['#e5071a', '#00CD00', '#1e90ff', '#FF9900', '#fd79a8', '#8074b2', '#636e72']
 
 # -------------------
 # algs = ['PPO', 'PPO_B_0', 'PPO_B_500', 'PPO_B_1k']  # * 给出算法文件夹名
-algs = ['PPO', 'PPO~detach~cvae~expert']  # * 给出算法文件夹名
+algs = ['PPO', 'PPO~cvae']  # * 给出算法文件夹名
 mission = 'sumo'
 target_index = 'Return'
 # -------------------
